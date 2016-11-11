0b76e96508b016cc7939450d8a6b22d2439997dd
Patrick Lewis <patrickml.pml@gmail.com>
2016-11-11 18:14:36 -0500

    PI Raspi

--- pi/src/index.js
+++ pi/src/index.js
@@ -1,5 +1,8 @@
 const five = require('johnny-five');
-const board = new five.Board();
+const Raspi = require('raspi-io');
+const board = new five.Board({
+  io: new Raspi(),
+});
 
 board.on('ready', () => {
   // Create a standard `led` component instance
