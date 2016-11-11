81758bf1e555ee32aa342773500405110a7d92ca
Patrick Lewis <patrickml.pml@gmail.com>
2016-11-11 15:17:02 -0500

    PI First Test

--- pi/src/index.js
+++ pi/src/index.js
@@ -1,1 +1,11 @@
+const five = require('johnny-five');
+const board = new five.Board();
 
+board.on('ready', () => {
+  // Create a standard `led` component instance
+  const led = new five.Led(13);
+  const led2 = new five.Led(12);
+
+  led.blink(500);
+  led2.blink(750);
+});
